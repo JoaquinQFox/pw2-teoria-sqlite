d9910f777f3ee978e07ddd60746592d9314621bf
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-18 13:27:20 -0500

    Agregue funcionalidad de paginacion para mostrar todos los datos de la tabla

--- front/script.js
+++ front/script.js
@@ -1,15 +1,53 @@
 const tableBody = document.getElementById('actorTableBody');
+const paginacion = document.getElementById('paginacion');
+
+const filasPorPagina = 20;
+let paginaActual = 1;
+let datos = [];
 
 fetch('./actores')
 .then(response => response.json())
 .then(data => {
-    console.log(data);
-    let output = '';
+    datos = data;
+    mostrarTabla();
+    mostrarPaginacion();
+})
+.catch(error => console.error(error));
 
-    for (let i = 0; i < 20; i++) {
-        output += `<tr><td>${data[i].Name}</td></tr>`
+function mostrarTabla() {
+    const indiceMenor = (paginaActual - 1) * filasPorPagina;
+    const indiceMayor = indiceMenor + filasPorPagina;
+    const paginaData = datos.slice(indiceMenor, indiceMayor);
+
+    tableBody.innerHTML = '';
+
+    let output = '';
+    for (let actor of paginaData) {
+        output += `<tr><td>${actor.Name}</td></tr>`
     }
 
     tableBody.innerHTML = output;
-})
-.catch(error => console.error(error));
+}
+
+function mostrarPaginacion() {
+    const paginas = Math.ceil(datos.length / filasPorPagina);
+    paginacion.innerHTML = '';
+    console.log(paginas);
+
+    for (let i = 1; i < paginas + 1; i++) {
+        const boton = document.createElement('button');
+
+        boton.textContent = i;
+        boton.classList.add('botonPagina');
+
+        if (i ==  paginaActual) boton.disabled = true;
+
+        boton.addEventListener('click', () => {
+            paginaActual = i;
+            mostrarTabla();
+            mostrarPaginacion();
+        });
+
+        paginacion.appendChild(boton);
+    }
+}
