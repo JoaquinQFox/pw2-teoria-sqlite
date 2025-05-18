a877c027bf5b4153025331e7736df91aabcf81b3
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-18 12:57:34 -0500

    Se agrego estilo basico para las tablas y se uso AJAX para mostrar los actores de la base de datos en la tabla

--- front/script.js
+++ front/script.js
@@ -1,6 +1,15 @@
+const tableBody = document.getElementById('actorTableBody');
+
 fetch('./actores')
 .then(response => response.json())
 .then(data => {
     console.log(data);
+    let output = '';
+
+    for (let i = 0; i < 20; i++) {
+        output += `<tr><td>${data[i].Name}</td></tr>`
+    }
+
+    tableBody.innerHTML = output;
 })
 .catch(error => console.error(error));
